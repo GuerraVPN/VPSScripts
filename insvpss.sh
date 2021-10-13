8c2cf13f801e3d2dee42a0d61e7ae16a7c5ed688
GuerraVPN <81371258+GuerraVPN@users.noreply.github.com>
2021-10-13 19:07:39 -0300

    Add files via upload

---
+++ insvpss.sh
@@ -0,0 +1,43 @@
+#CONFIGURACAO DE INSTALACAO
+clear
+echo -e "
+############################
+#                          #
+# INSTALAR O VPSSCRIPT ??? #
+#                          #
+############################
+"
+echo -e "
+(1) SIM
+(2) NAO
+"
+
+echo -ne "E entao? :"; read x
+case "$x" in
+    1|01)
+   clear
+   mkdir /etc/vpsscript
+   clear
+   apt-get update -y > /etc/null; apt-get upgrade -y > /etc/null; wget /etc/vpsscript https://raw.githubusercontent.com/GuerraVPN/VPSScripts/main/VPSScript.sh > /etc/null
+   clear
+   wget /etc/vpsscript https://raw.githubusercontent.com/GuerraVPN/VPSScripts/main/vpsscriptatt > /etc/null
+   mv /etc/vpsscript/VPSScript.sh /etc/vpsscript/VPSScript > /etc/null
+   clear;
+   ;;
+    2|02)
+   clear
+   exit;
+   ;;
+esac
+   echo -ne "PROSSEGUIR PARA O MENU? (S|N): "; read x
+case "$x" in
+    S)
+   clear
+   bash /etc/vpsscript/VPSScript
+   ;;
+    N)
+   clear
+   exit;
+   ;;
+esac
+#fim
